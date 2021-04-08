f60592ee8cb527c27bc9e8e2df7cdce0fe9dc1fe
ckm1nodoma <82160887+ckm1nodoma@users.noreply.github.com>
2021-04-08 06:48:51 -0700

    Add files via upload

---
+++ Playground/MyFirstPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var hello = "Hello, "
+var name = "Nikita"
+var age = "17"
+
+var result = hello + "my name is " + name
+result = result + "\nI am \(age) years old"
+result = result + "\nI love coding"
+print(result)
